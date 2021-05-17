33e657b6a31b75e56d176584eff79cc4e28fa25f
andreasfedermannkm <andreas.federmann@kraussmaffei.com>
2021-05-17 20:32:58 +0200

    Remove unused variables from create-deployment-status

--- create-deployment-status/dist/index.js
+++ create-deployment-status/dist/index.js
@@ -229,15 +229,15 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 async function main() {
-    const {context} = github;
+    const { context } = github;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
-    const token = core.getInput("github-token", {required: true});
-    const url = core.getInput("target-url", {required: false}) || defaultUrl;
-    const description = core.getInput("description", {required: false}) || "";
-    const deploymentId = core.getInput("deployment-id", {required: true});
-    const environmentUrl = core.getInput("environment-url", {required: false}) || "";
-    const state = core.getInput("state", {required: true});
-    const octokit = github.getOctokit(token, {previews: ["flash", "ant-man"]});
+    const token = core.getInput("github-token", { required: true });
+    const url = core.getInput("target-url", { required: false }) || defaultUrl;
+    const description = core.getInput("description", { required: false }) || "";
+    const deploymentId = core.getInput("deployment-id", { required: true });
+    const environmentUrl = core.getInput("environment-url", { required: false }) || "";
+    const state = core.getInput("state", { required: true });
+    const octokit = github.getOctokit(token, { previews: ["flash", "ant-man"] });
 
 
     const req = {
@@ -251,8 +251,7 @@ async function main() {
         environment_url: environmentUrl
     }
     console.log('createDeploymentStatus %o', req);
-    const resp = await octokit.repos.createDeploymentStatus(req);
-
+    await octokit.repos.createDeploymentStatus(req);
 }
 
 main().catch(function (error) {
--- create-deployment-status/index.js
+++ create-deployment-status/index.js
@@ -2,15 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function main() {
-    const {context} = github;
+    const { context } = github;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
-    const token = core.getInput("github-token", {required: true});
-    const url = core.getInput("target-url", {required: false}) || defaultUrl;
-    const description = core.getInput("description", {required: false}) || "";
-    const deploymentId = core.getInput("deployment-id", {required: true});
-    const environmentUrl = core.getInput("environment-url", {required: false}) || "";
-    const state = core.getInput("state", {required: true});
-    const octokit = github.getOctokit(token, {previews: ["flash", "ant-man"]});
+    const token = core.getInput("github-token", { required: true });
+    const url = core.getInput("target-url", { required: false }) || defaultUrl;
+    const description = core.getInput("description", { required: false }) || "";
+    const deploymentId = core.getInput("deployment-id", { required: true });
+    const environmentUrl = core.getInput("environment-url", { required: false }) || "";
+    const state = core.getInput("state", { required: true });
+    const octokit = github.getOctokit(token, { previews: ["flash", "ant-man"] });
 
 
     const req = {
@@ -24,8 +24,7 @@ async function main() {
         environment_url: environmentUrl
     }
     console.log('createDeploymentStatus %o', req);
-    const resp = await octokit.repos.createDeploymentStatus(req);
-
+    await octokit.repos.createDeploymentStatus(req);
 }
 
 main().catch(function (error) {
